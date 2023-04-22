3c6a245f0dab885197d821d200acfe9c05b04004
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-22 12:45:00 -0700

    Code transferred

---
+++ src/CeballosMelrose11.java
@@ -0,0 +1,29 @@
+// Importing Java class to create array list object
+import java.util.ArrayList;
+
+/**
+ * To render a music playlist using java Arraylist 
+ * 
+ * @author     Ceballos, Melrose
+ * @assignment ICS 111 Assignment 11
+ * @date       April 15, 2023
+ * @bugs       I think I'm having a hard time understanding the print method
+ *             I also was confused on how to implement the time and date
+ */
+ 
+public class CeballosMelrose11{
+   public static void main(String[] args){
+   // Creates array list
+   ArrayList<String> songs = new ArrayList<>();
+   songs.add(new Song("Honey", "Beyonce", 312));
+   songs.add("Alone", "Doja Cat", 300));
+   songs.add("Wine Pon You", "Doja Cat", 245));
+   songs.add("Alien Superstar", "Beyonce", 450));
+   songs.add("More Than A Woman", "Bee Gees", 245));
+  
+   
+   int i = songs
+   for (int i = 0; i<songs.length; i++){
+      System.out.println(i)
+   }
+}
